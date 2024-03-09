f7cb47d3b898fe3195019c883372e6ffab859264
Jonathan <jonathanguven@gmail.com>
2024-03-09 15:23:18 -0800

    proposed db schemas

--- server/schemas/event.js
+++ server/schemas/event.js
@@ -2,5 +2,33 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const eventSchema = new Schema({
-    //need friends field, simply an array _id of friends
-})
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    participants: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Profile',
+        required: true,
+    }],
+    date: {
+        type: Date,
+        required: true,
+    },
+    start: {
+        type: Number, // 0-96, 0 is midnight, 96 is 11:45 PM
+        required: true,
+    },
+    end: {
+        type: Number, // 0-96, 0 is midnight, 96 is 11:45 PM
+        required: true,
+    },
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+export default Event;
+
--- server/schemas/profile.js
+++ server/schemas/profile.js
@@ -1,6 +1,41 @@
 import mongoose from 'mongoose'
 const { Schema } = mongoose
 
+const Day = new Schema({
+    slots: {
+        type: [Boolean],
+        default: () => Array(96).fill(false), // 96 x 15 minute time slots in a day
+    },
+}, { _id: false });
+
+const Week = new Schema({
+    sunday: Day, 
+    monday: Day, 
+    tuesday: Day,
+    wednesday: Day,
+    thursday: Day,
+    friday: Day, 
+    saturday: Day,
+}, { _id: false });
+
 const profileSchema = new Schema({
     //need friends field, simply an array _id of friends
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    schedule: Week,
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Profile',
+    }],
 })
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+export default Profile;
