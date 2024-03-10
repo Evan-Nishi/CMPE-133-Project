c0b0f8a5e2c9c545808c36a421e3483df7174f1b
food0903 <phuclthg@gmail.com>
2024-03-09 18:14:27 -0800

    navbar prototype

---
+++ client/src/Pages/AboutPages/AboutPage.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our About page. We are a fantastic company doing amazing things!</p>
+    </div>
+  );
+};
+
+export default About;
