f9ea8ea254794dcc53b2c87c8e2284c100a9b5cf
Jonathan <jonathanguven@gmail.com>
2024-04-10 23:03:53 -0700

    implemented authenticate middleware function

--- server/routes/userSession.js
+++ server/routes/userSession.js
@@ -2,23 +2,12 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
+import { authenticate } from '../middleware/auth.js';
+
 const router = express.Router();
 
-router.get('/session', (req, res) => {
-    try{
-        const token = req.cookies.jwt;
-        if (!token) {
-            return res.json({ user: null });
-        }
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {
-                return res.json({ user: null });
-            }
-            res.json({ user: decoded });
-        });   
-    } catch (error) {
-        res.json({ user: null });
-    }
+router.get('/session', authenticate, (req, res) => {
+    res.json({ user: { id: req.id } });
 });
 
 export default router;
